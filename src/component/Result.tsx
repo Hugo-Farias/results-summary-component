a1a7a9bbff52d2dfeed46e89226b5bf319e96a16
Hugo-Farias <hg80rod@gmail.com>
2023-02-26 12:11:36 -0300

    "Result" Completed

--- src/component/Result.tsx
+++ src/component/Result.tsx
@@ -2,13 +2,15 @@ import cls from "./Result.module.css";
 
 const Result = function () {
   return (
-    <div className={cls.main}>
-      <h2 className={cls.title}>Your Result</h2>
-      <div className={cls.score}>
+    <div className={`${cls.main} flex-column`}>
+      <h2>Your Result</h2>
+
+      <div className={`${cls.score} flex-column`}>
         <div className={cls.userScore}>76</div>
         <span className={cls.totalScore}>of 100</span>
       </div>
-      <div>
+
+      <div className={`${cls.result} flex-column`}>
         <h1>Great</h1>
         <p>
           You scored higher than 65% of the people who have taken these tests.
